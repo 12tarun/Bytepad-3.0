0e139715fb00e0b9120748c2519eee375f173e3b
Tarun Gupta <12tarungupta@gmail.com>
2018-11-30 22:28:26 +0530

    add controller to get list of all subjects on client side

---
+++ Bytepad 3.0/Controllers/subject_detailController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bytepad_3._0.Models;
+
+namespace Bytepad_3._0.Controllers
+{
+    public class subject_detailController : ApiController
+    {
+        private ISubject _subject = null;
+
+        public subject_detailController(ISubject subject)
+        {
+            _subject = subject;
+        }
+
+        // Gets list of all subjects on the client side so that it can be used in search on frontend.
+        [HttpGet]
+        [Route("api/subject_detail")]
+        public IHttpActionResult GetSubjects()
+        {
+            List<Subject> dataAllSubjects = _subject.GetAllSubjects();
+            return Ok(dataAllSubjects);
+        }
+    }
+}
