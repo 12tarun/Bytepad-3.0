055c8d38d2fbc078d37e941b963157ccad69f0a5
suryakant1698 <suryakant.rocky@gmail.com>
2019-01-03 17:02:29 +0530

    complete api

--- Bytepad 3.0/App_Start/WebApiConfig.cs
+++ Bytepad 3.0/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -22,7 +23,11 @@ namespace Bytepad_3._0
             );
 
             EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
-
+            config.EnableCors(cors);
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
     }
 }
